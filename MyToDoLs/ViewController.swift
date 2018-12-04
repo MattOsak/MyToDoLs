e5a752b422880c32fd9ef7b3204c32ef5f9b8188
MattOsak <matt.osak@gmail.com>
2018-12-04 18:27:16 -0500

    Initial Commit

---
+++ MyToDoLs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyToDoLs
+//
+//  Created by Matt Osak on 2018-12-04.
+//  Copyright © 2018 Matt Osak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
